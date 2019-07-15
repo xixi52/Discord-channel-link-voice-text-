e960e4a228fe4cda2744ef76b6c5042d1fa58df5
xixi52 <51019403+xixi52@users.noreply.github.com>
2019-07-16 01:01:31 +0200

    Script principal

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = 'XXXXXXXXXXXXXXXXX';
+
+
+bot.on("ready",()=>{
+    console.log(`Connecté en tant que ${client.user.tag}!`);
+});
+
+
+
+bot.on("voiceStateUpdate",(ancien,nouveau)=>{
+
+    var texte = nouveau.guild.channels.get('ID-SALON-TEXTE');
+
+
+    if(nouveau.voiceChannelID == 'ID-SALON-VOCAL'){
+
+
+      texte.overwritePermissions(nouveau,{
+        "SEND_MESSAGES":true,
+        "READ_MESSAGES":true,
+});
+    }else{
+
+        texte.overwritePermissions(nouveau,{
+          "SEND_MESSAGES":false,
+          "READ_MESSAGES":false,
+  });
+    }
+});
+
+bot.login(token);
